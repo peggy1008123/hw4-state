c063785a4332f00ba1622e54b05261df72233aae
peggy1008123 <peggy1008123@yahoo.com.tw>
2021-03-22 22:14:34 +0800

    burger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,117 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Drawer, Button } from "antd";
 
 export default function NavBar() {
-    return (
-        <div className="nav-bar">
-            <NavLink to="/tableware" className="nav-item" activeClassName="nav-item--active">
-                Tableware
-            </NavLink>
-            <NavLink to="/cookware" className="nav-item" activeClassName="nav-item--active">
-                Cookware
-            </NavLink>
-            <NavLink to="/home-accessories" className="nav-item" activeClassName="nav-item--active">
-                Home accessories
-            </NavLink>
-            <NavLink to="/lighting" className="nav-item" activeClassName="nav-item--active">
-                Lighting
-            </NavLink>
-            <NavLink to="/textile" className="nav-item" activeClassName="nav-item--active">
-                Textile
-            </NavLink>
-            <NavLink to="/furniture" className="nav-item" activeClassName="nav-item--active">
-                Furniture
-            </NavLink>
-        </div>
-    );
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
+  return (
+    <div>
+      <div className="nav-bar">
+        <NavLink
+          to="/tableware"
+          className="nav-item"
+          activeClassName="nav-item--active"
+        >
+          Tableware{" "}
+        </NavLink>{" "}
+        <NavLink
+          to="/cookware"
+          className="nav-item"
+          activeClassName="nav-item--active"
+        >
+          Cookware{" "}
+        </NavLink>{" "}
+        <NavLink
+          to="/home-accessories"
+          className="nav-item"
+          activeClassName="nav-item--active"
+        >
+          Home accessories{" "}
+        </NavLink>{" "}
+        <NavLink
+          to="/lighting"
+          className="nav-item"
+          activeClassName="nav-item--active"
+        >
+          Lighting{" "}
+        </NavLink>{" "}
+        <NavLink
+          to="/textile"
+          className="nav-item"
+          activeClassName="nav-item--active"
+        >
+          Textile{" "}
+        </NavLink>{" "}
+        <NavLink
+          to="/furniture"
+          className="nav-item"
+          activeClassName="nav-item--active"
+        >
+          Furniture{" "}
+        </NavLink>{" "}
+      </div>
+      <div className="mobileVisible">
+        <Button className="btn" type="primary" onClick={showDrawer}>
+          &#9776;
+        </Button>
+        <Drawer
+          placement="left"
+          closable={false}
+          onClose={onClose}
+          visible={visible}
+        >
+          <div className="hamburger-wrap">
+            <NavLink
+              to="/tableware"
+              className="hamburger-item"
+              activeClassName="nav-item--active"
+            >
+              Tableware{" "}
+            </NavLink>{" "}
+            <NavLink
+              to="/cookware"
+              className="hamburger-item"
+              activeClassName="nav-item--active"
+            >
+              Cookware{" "}
+            </NavLink>{" "}
+            <NavLink
+              to="/home-accessories"
+              className="hamburger-item"
+              activeClassName="nav-item--active"
+            >
+              Home accessories{" "}
+            </NavLink>{" "}
+            <NavLink
+              to="/lighting"
+              className="hamburger-item"
+              activeClassName="nav-item--active"
+            >
+              Lighting{" "}
+            </NavLink>{" "}
+            <NavLink
+              to="/textile"
+              className="hamburger-item"
+              activeClassName="nav-item--active"
+            >
+              Textile{" "}
+            </NavLink>{" "}
+            <NavLink
+              to="/furniture"
+              className="hamburger-item"
+              activeClassName="nav-item--active"
+            >
+              Furniture{" "}
+            </NavLink>{" "}
+          </div>
+        </Drawer>
+      </div>
+    </div>
+  );
 }
